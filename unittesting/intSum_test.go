d5971a86c02c8ce39fd2b1808162ce610292056a
harsha-fsd <sriharsha.fsd@gmail.com>
2021-10-01 20:58:18 +0530

    added before and after test code

--- unittesting/intSum_test.go
+++ unittesting/intSum_test.go
@@ -1,9 +1,21 @@
 package test
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 func add(x int, y int) int {
 	return x + y
+}
+
+//invoked only once but not before and after each test. Used for setup and teardown
+func TestMain(m *testing.M) {
+	fmt.Println("Setup stuff goes here. ")
+	exitValue := m.Run()
+	fmt.Println("Cleanup stuff goes here.")
+	os.Exit(exitValue)
 }
 
 func Test_add(t *testing.T) {
