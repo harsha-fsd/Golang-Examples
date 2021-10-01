97357b7a3c246e555eb9ce6d79f69518ef7edbd0
harsha-fsd <sriharsha.fsd@gmail.com>
2021-10-01 11:15:36 +0530

    sample unit test

--- unittesting/intSum_test.go
+++ unittesting/intSum_test.go
@@ -3,16 +3,12 @@ package test
 import "testing"
 
 func add(x int, y int) int {
-	if x >= 0 && y >= 0 {
-		return x + y
-	}
-	return 0
-
+	return x + y
 }
 
 func Test_add(t *testing.T) {
 	sum := add(1, 3)
-	if sum != 5 {
+	if sum != 4 {
 		t.Error("wrong sum")
 	}
 
